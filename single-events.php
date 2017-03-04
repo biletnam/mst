9c4a69252614460b9c5d9ea707dc6e6211ffa16b
Suman Ali <sumanengbd@gmail.com>
2017-03-04 19:15:25 +0600

    single events page

--- single-events.php
+++ single-events.php
@@ -9,7 +9,47 @@
 
 get_header(); ?>
 
+	<div id="purchase" class="with-images">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<div class="entry-header text-center">
+						<h3 class="entry-title"><span  style="color: <?php echo getTaxField('store_color'); ?>">TORONTO RAPTORS</span> vs DETORIT PISTONS</h3>
+					</div>
+				</div>
+			</div>
 
-<h1>This is single events.</h1>
+			<div class="row">
+
+				<!-- purchase-item -->
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<div class="col-md-6 col-sm-6 col-xs-12">
+						<div class="purchase-item-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/Purchase.png" class="img-responsive" alt="">
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-12">
+						<div class="purchase-item text-center">
+							<h4 class="title" style="color: <?php echo getTaxField('store_color'); ?>">Section 112, Row 21<br> Seats 1-2</h4>
+							<span class="price">$220</span>
+							<h4 class="remaining-quantity">Only <span  style="color: #1E3F7C;">8</span> Left</h4>
+
+							<form class="cart form-inline" method="post">
+							 	<div class="form-group">
+							 		<label for="quantity">Quantity:</label>
+							 		<div class="quantity">
+										<input type="number" name="quantity" id="quantity" value="1" class="qty" size="4">
+									</div>
+		 							<button type="submit" class="single_add_to_cart_button button"  style="background-color: <?php echo getTaxField('store_color'); ?>">Buy Now</button>
+							 	</div>
+							</form>
+						</div>
+					</div><!-- /purchase-item -->
+				</div>
+
+				<style type="text/css">.purchase-item:before{border-color: <?php echo getTaxField('store_color'); ?>}</style>
+			</div><!-- /row -->
+		</div>
+	</div>
 
 <?php get_footer();
