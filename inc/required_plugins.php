cfd4bb1e2e55946a36bd4cf4703580d41eeb930f
Suman Ali <sumanengbd@gmail.com>
2017-03-07 12:10:18 +0600

    post and tax re-order plugin add

--- inc/required_plugins.php
+++ inc/required_plugins.php
@@ -32,6 +32,16 @@ function mst_register_required_plugins() {
 			'name'      	=> 'Contact Form 7',
 			'slug'      	=> 'contact-form-7',
 			'required'  	=> true,
+		),
+		array(
+			'name'      	=> 'Post Types Order',
+			'slug'      	=> 'post-types-order',
+			'required'  	=> true,
+		),
+		array(
+			'name'      	=> 'WP Term Order',
+			'slug'      	=> 'wp-term-order',
+			'required'  	=> true,
 		)
 	);
 
